1b2a372bd9751150a73a91a000740f19639c01fc
DanxzSleepy <wolfdangalaxy2@gmail.com>
2025-07-24 16:48:19 -0300

    Fiz muita coisa nisso adiconei banco de dados com foto reposividad eresolvi o problema do placeholder junto com as barra de pesquisa

--- Nerdhub/settings.py
+++ Nerdhub/settings.py
@@ -124,3 +124,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'nucleo/static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
