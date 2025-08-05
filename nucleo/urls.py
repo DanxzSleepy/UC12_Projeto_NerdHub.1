2be809946dd8d6407b77cbfb51143428a234c127
Lucas Eduardo dos Anjos Rodrigues <Lucas54127636@edu.df.senac.br>
2025-08-05 16:15:04 -0300

    UPDATE
    criação da tela de conta

--- nucleo/urls.py
+++ nucleo/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -6,5 +6,10 @@ urlpatterns = [
     path('produto/<int:id>/', views.detalhe_produto, name='detalhe_produto'),
     path('sobre/', views.sobre, name='sobre'),
     path('suporte/', views.suporte, name='suporte'),
+    path('usuario/', include('usuarios.urls'), name='usuarios')
 
+<<<<<<< Updated upstream
 ] 
+=======
+] 
+>>>>>>> Stashed changes
