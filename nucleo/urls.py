50d1b886216c80824d3fbcfefd3058d5464aa982
DanxzSleepy <wolfdangalaxy2@gmail.com>
2025-08-05 14:23:04 -0300

    paginas sobre e suporte feitas e alteracoes e arquivos.py

--- nucleo/urls.py
+++ nucleo/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('produto/<int:id>/', views.detalhe_produto, name='detalhe_produto'),
-]
+    path('sobre/', views.sobre, name='sobre'),
+    path('suporte/', views.suporte, name='suporte'),
+
+] 
--- nucleo/views.py
+++ nucleo/views.py
@@ -3,8 +3,17 @@ from .models import Produto
 
 def index(request):
     produtos = Produto.objects.all()
-    return render(request, 'nucleo/index.html', {'produtos': produtos})
+    return render(request, 'nucleo/index.html', {'produtos': produtos, 'page_name': 'index'})
+
+def sobre(request):
+    return render(request, 'nucleo/sobre.html', {'page_name': 'sobre'})
+
+def suporte(request):
+    return render(request, 'nucleo/suporte.html', {'page_name': 'suporte'})
 
 def detalhe_produto(request, id):
     produto = get_object_or_404(Produto, id=id)
-    return render(request, 'nucleo/detalhe_produto.html', {'produto': produto})
+    return render(request, 'nucleo/detalhe_produto.html', {
+        'produto': produto,
+        'page_name': 'detalhe_produto'  # Opcional, se quiser usar
+    })
